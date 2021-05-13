4aeb167c9bed4d50e16c4f54b5f39e9498db22f0
reb3rry <michalmalinowski99@yahoo.co.uk>
2021-05-13 21:30:58 +0200

    Project init

---
+++ src/phong/Controller.java
@@ -0,0 +1,29 @@
+package phong;
+
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.paint.Color;
+import javafx.util.Pair;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+import static java.lang.Math.abs;
+
+public class Controller {
+
+    @FXML
+    private Canvas viewport;
+
+    public void initialize() {
+
+    }
+
+    @FXML
+    public void readKeys(KeyEvent e) {
+
+    }
+}
